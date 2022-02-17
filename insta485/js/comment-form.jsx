1970bacbe8c5e6b2bad831c13caa486f7f5ebce4
yhuo <yhuo@umich.edu>
2022-02-17 14:17:39 -0500

    file structure setup

---
+++ insta485/js/comment-form.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function CommentForm() {
+  return (
+    <form className="comment-form">
+      <input type="text" value="" />
+    </form>
+  );
+}
---
+++ insta485/js/delete-comment-button.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function DeleteCommentButton() {
+  return (
+    <button type="button" className="delete-comment-button">
+      Delete comment
+    </button>
+  );
+}
