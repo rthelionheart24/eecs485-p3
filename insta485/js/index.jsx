74c918dcb43b03b3540e6fddc630c9979b6975b2
Qifa Wang <qifwang@umich.edu>
2022-02-17 23:03:39 -0500

    Top-level index.jsx completed

---
+++ insta485/js/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Post from './post';
+import Header from './header';
+
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      next: '',
+      results: '',
+      url: '',
+    };
+  }
+
+  componentDidMount() {
+    fetch('api/v1/posts/', { credentials: 'same-origin' })
+      .then((response) => {
+        if (!response.ok) throw Error(response.statusText);
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          next: data.next,
+          results: data.results,
+          url: data.url,
+        });
+      })
+      .catch((error) => console.log(error));
+  }
+
+  render() {
+    const { next, results, url } = this.state;
+    const feed = results.map((post) => (<Post url={post.url} />));
+
+    return (
+      <>
+        <Header />
+        <div>
+          {feed}
+        </div>
+      </>
+    );
+  }
+}
+
+export default Index;
